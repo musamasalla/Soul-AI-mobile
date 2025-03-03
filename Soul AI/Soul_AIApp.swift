84aef90ffa87a88ab55904f1380003ec9b211f8b
try-hard1010 <136584339+musamasalla@users.noreply.github.com>
2025-03-03 13:13:01 +0200

    Initial Commit

---
+++ Soul AI/Soul_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soul_AIApp.swift
+//  Soul AI
+//
+//  Created by Musa Masalla on 2025/03/03.
+//
+
+import SwiftUI
+
+@main
+struct Soul_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Soul AITests/Soul_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Soul_AITests.swift
+//  Soul AITests
+//
+//  Created by Musa Masalla on 2025/03/03.
+//
+
+import Testing
+@testable import Soul_AI
+
+struct Soul_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
